76812d2e04da13bd258d499ba936cef26dfc864d
RamiroTelles <yenyogamer290@gmail.com>
2024-10-19 22:36:09 -0600

    grpc golang

---
+++ Proyecto2/gRPC/golang-client/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"flag"
+	pb "golang-client/proto"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr1 = flag.String("addr1", "localhost:50051", "the address to connect to")
+	addr2 = flag.String("addr2", "localhost:50052", "the address to connect to")
+	addr3 = flag.String("addr3", "localhost:50053", "the address to connect to")
+)
+
+type Student struct {
+	Name       string `json:"name"`
+	Age        int    `json:"age"`
+	Faculty    string `json:"faculty"`
+	Discipline int    `json:"discipline"`
+}
+
+func sendData(fiberCtx *fiber.Ctx) error {
+	var body Student
+	if err := fiberCtx.BodyParser(&body); err != nil {
+		return fiberCtx.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	address := *addr1
+	if body.Discipline == 2 {
+		address = *addr2
+	} else if body.Discipline == 3 {
+		address = *addr3
+	}
+
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewStudentClient(conn)
+
+	// Create a channel to receive the response and error
+	responseChan := make(chan *pb.StudentResponse)
+	errorChan := make(chan error)
+
+	go func() {
+
+		// Contact the server and print out its response.
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		r, err := c.SendStudent(ctx, &pb.StudentRequest{
+			Name:       body.Name,
+			Age:        int32(body.Age),
+			Faculty:    body.Faculty,
+			Discipline: pb.Discipline(body.Discipline),
+		})
+
+		if err != nil {
+			errorChan <- err
+			return
+		}
+
+		responseChan <- r
+	}()
+
+	select {
+	case response := <-responseChan:
+		return fiberCtx.JSON(fiber.Map{
+			"message": response.GetSuccess(),
+		})
+	case err := <-errorChan:
+		return fiberCtx.Status(500).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	case <-time.After(5 * time.Second):
+		return fiberCtx.Status(500).JSON(fiber.Map{
+			"error": "timeout",
+		})
+	}
+}
+
+func main() {
+	app := fiber.New()
+	app.Post("/faculty", sendData)
+
+	err := app.Listen(":8080")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
---
+++ Proyecto2/gRPC/golang-server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "golang-server/proto"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50052, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedStudentServer
+}
+
+func (s *server) SendStudent(_ context.Context, in *pb.StudentRequest) (*pb.StudentResponse, error) {
+	log.Printf("Received: %v", in)
+	log.Printf("Student name: %s", in.GetName())
+	log.Printf("Student faculty: %s", in.GetFaculty())
+	log.Printf("Student age: %d", in.GetAge())
+	log.Printf("Student discipline: %d", in.GetDiscipline())
+
+	return &pb.StudentResponse{
+		Success: true,
+	}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterStudentServer(s, &server{})
+	log.Printf("Server started on port %d", *port)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
